9f299f7575d7c8b2aa4a1300555df916681be16b
Huijeong Kim <herehuijeong@gmail.com>
2023-05-03 22:54:31 +0900

    Refactor run_mode

--- src/config.rs
+++ src/config.rs
@@ -23,23 +23,31 @@ pub struct DeviceConfig {
 pub enum RunMode {
     Development,
     Production,
-    /// Custom run mode has test name. There should be a config file in config folder named with test name
-    Custom(String),
+    /// In test mode, configuration file for this test should be provided
+    Test(String),
 }
 impl std::fmt::Display for RunMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
             RunMode::Development => write!(f, "development"),
             RunMode::Production => write!(f, "production"),
-            RunMode::Custom(mode) => write!(f, "{}", mode),
+            RunMode::Test(_) => write!(f, "Test"),
         }
     }
 }
-
+impl RunMode {
+    fn get_config_file(&self) -> &str {
+        match self {
+            RunMode::Development => "config/development.toml",
+            RunMode::Production => "config/production.toml",
+            RunMode::Test(file) => file,
+        }
+    }
+}
 pub fn get_config(run_mode: &RunMode) -> Result<MinistoreConfig, String> {
     let config = Config::builder()
         .add_source(File::with_name("config/default.toml"))
-        .add_source(File::with_name(&format!("config/{}.toml", run_mode)))
+        .add_source(File::with_name(run_mode.get_config_file()))
         .build()
         .map_err(|e| e.to_string())?;
 
@@ -50,6 +58,8 @@ pub fn get_config(run_mode: &RunMode) -> Result<MinistoreConfig, String> {
 
 #[cfg(test)]
 mod tests {
+    use std::io::Write;
+
     use super::*;
 
     #[test]
@@ -66,5 +76,32 @@ mod tests {
 
         assert_eq!(config.log.level, "info");
         assert_eq!(config.devices.use_fake, false);
+    }
+
+    #[test]
+    fn configuring_with_test_mode_should_success() {
+        let test_config_str = r#"[log]
+    level = "debug"
+
+    [devices]
+    use_fake = true
+    fake_device_location = "."
+    fake_device_type = "SimpleFake"
+        "#;
+
+        let test_config_file = "config/configuring_with_test_mode_should_success.toml";
+        let mut file = std::fs::File::create(test_config_file).expect("Failed to create test file");
+        file.write(test_config_str.as_bytes())
+            .expect("Failed to write test config file");
+
+        let config = get_config(&RunMode::Test(test_config_file.to_string()));
+        let config = config.unwrap();
+
+        assert_eq!(config.log.level, "debug");
+        assert_eq!(config.devices.use_fake, true);
+        assert_eq!(config.devices.fake_device_location, ".");
+        assert_eq!(config.devices.fake_device_type, "SimpleFake");
+
+        std::fs::remove_file(test_config_file).expect("Failed to remove test file");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,11 @@ pub mod utils;
 
 use self::config::RunMode;
 
-pub fn start(devel: bool, test_name: Option<&String>) -> Result<RunMode, String> {
-    let run_mode = get_run_mode(devel, test_name);
+pub fn start(devel: bool, test_configfile: Option<&String>) -> Result<RunMode, String> {
+    let run_mode = get_run_mode(devel, test_configfile);
     let config = config::get_config(&run_mode)?;
+
+    println!("run_mode: {}", run_mode);
     println!("config: {:#?}", config);
 
     // Do something here..
@@ -20,7 +22,7 @@ pub fn start(devel: bool, test_name: Option<&String>) -> Result<RunMode, String>
 
 fn get_run_mode(devel: bool, test_name: Option<&String>) -> RunMode {
     if test_name.is_some() {
-        RunMode::Custom(test_name.unwrap().clone())
+        RunMode::Test(test_name.unwrap().clone())
     } else if devel == true {
         RunMode::Development
     } else {
@@ -42,10 +44,10 @@ mod test {
 
     #[test]
     fn ministore_should_run_with_test_mode_when_test_name_provided() {
-        let test_name = "production".to_string(); // temporally use exisiting config file name
-        let result = start(false, Some(&test_name));
+        let test_config = "config/production.toml".to_string(); // temporally use exisiting config file name
+        let result = start(false, Some(&test_config));
 
-        assert_eq!(result.is_ok(), true);
-        assert_eq!(result.unwrap(), RunMode::Custom(test_name));
+        assert_eq!(result.is_ok(), true, "{:?}", result);
+        assert_eq!(result.unwrap(), RunMode::Test(test_config));
     }
 }
